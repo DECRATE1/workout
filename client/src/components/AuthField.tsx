4cbedd5fb7a1347ca76f51cfef3dda759669510a
1DECRATE1 <nukegame12@gmail.com>
2025-03-07 20:19:33 +0300

    close to complete registration ui

--- client/src/components/AuthField.tsx
+++ client/src/components/AuthField.tsx
@@ -3,14 +3,17 @@ import { ChangeEvent } from "react";
 export default function AuthField({
   handle,
   value,
+  placeholder,
 }: {
   handle: (e: ChangeEvent<HTMLInputElement>) => void;
   value: string;
+  placeholder: string;
 }) {
   return (
     <input
       type="text"
-      className="bg-black w-[571px] h-[78px] rounded-4xl text-white text-[24px] pl-2 outline-0"
+      className="bg-black w-[571px] h-[78px] rounded-4xl text-white text-[24px] pl-2 outline-0 select-all placeholder-amber-600"
+      placeholder="sex"
       value={value}
       onChange={(e) => handle(e)}
     />
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import AuthField from "@/components/AuthField";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, MouseEvent, useState } from "react";
+import { useFormState } from "react-dom";
 
 export default function Auth() {
   const [username, setUsername] = useState<string>("");
@@ -16,17 +17,44 @@ export default function Auth() {
   const handlePassword = (e: ChangeEvent<HTMLInputElement>): void => {
     setPassword(e.target.value);
   };
+
+  const submitAuth = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("hello world");
+  };
   return (
     <div className="flex w-screen h-screen bg-black items-center justify-center">
-      <div className="bg-[#FA1059] w-[706px] h-[943px] rounded-[51px] flex flex-col items-center scale-[0.85]">
-        <span className="mt-40 mb-20 font-bold text-[60px] text-white">
+      <div className="bg-[#FA1059] w-[706px] h-[943px] rounded-[51px] flex flex-col items-center scale-[0.8]">
+        <span className="mt-40 mb-20 font-bold text-[60px] text-white select-none">
           РЕГИСТРАЦИЯ
         </span>
-        <div className="flex flex-col gap-[42px]">
-          <AuthField handle={handleUsername} value={username}></AuthField>
-          <AuthField handle={handleEmail} value={email}></AuthField>
-          <AuthField handle={handlePassword} value={password}></AuthField>
-        </div>
+        <form
+          className="gap-[42px] flex flex-col items-center group:text-black [&>input]:uppercase"
+          onSubmit={(e) => submitAuth(e)}
+        >
+          <input
+            placeholder="Email"
+            type="email"
+            className="bg-black font-bold w-[571px] h-[78px] rounded-4xl text-white text-[28px] pl-2 outline-0 placeholder-white focus:text-transparent focus:[text-shadow:0px_0px_0px_white] focus:placeholder:opacity-0"
+          ></input>
+          <input
+            placeholder="Password"
+            type="email"
+            className="bg-black w-[571px] font-bold h-[78px] rounded-4xl text-white text-[28px] pl-2 outline-0 placeholder-white focus:text-transparent focus:[text-shadow:0px_0px_0px_white] focus:placeholder:opacity-0"
+          ></input>
+          <input
+            placeholder="Password Again"
+            type="email"
+            className="bg-black w-[571px] font-bold h-[78px] rounded-4xl text-white text-[28px] pl-2 outline-0 placeholder-white focus:text-transparent focus:[text-shadow:0px_0px_0px_white] focus:placeholder:opacity-0"
+          ></input>
+
+          <button
+            type="submit"
+            className="mt-10 p-1 bg-black rounded-4xl text-white text-[28px] px-8 font-bold py-2 cursor-pointer"
+          >
+            ПОТВЕРДИТЬ
+          </button>
+        </form>
       </div>
     </div>
   );
